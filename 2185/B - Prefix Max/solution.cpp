ce84dce6c8778fc31b6bffbaaa565906311a4572
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:52:40 +0600

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin >> n;
+    long long max_val = 0;
+    for (int i = 0; i < n; ++i) {
+        long long x;
+        cin >> x;
+            if (x > max_val) 
+            max_val = x;
+    }
+        cout << max_val * n << endl;
+    }    
+}
