9b9270e20ed30e62ff050be70aa349b82a4708a2
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:03:53 +0600

    Add Expensive Number [B] from Codeforces

---
+++ 2093/B - Expensive Number/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        string s;
+        cin >> s;
+ 
+        int zeros = 0;
+        int best = 0;
+ 
+        for (char c : s) {
+            if (c == '0') {
+                zeros++;
+            } else {
+                best = max(best, zeros + 1);
+            }
+        }
+ 
+        cout << s.size() - best << '
+';
+    }
+ 
+    return 0;
+}
