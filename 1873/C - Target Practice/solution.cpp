6c59939d5684b7bd430b9715df9777226d2a19d0
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:59:23 +0600

    Add Target Practice [C] from Codeforces

---
+++ 1873/C - Target Practice/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int score = 0;
+        
+        for (int i = 0; i < 10; ++i) {
+            string row;
+            cin >> row;
+            
+            for (int j = 0; j < 10; ++j) {
+                if (row[j] == 'X') {
+                    int points = min({i, j, 9 - i, 9 - j}) + 1;
+                    score += points;
+                }
+            }
+        }
+        
+        cout << score << "
+";
+    }
+ 
+    return 0;
+}
