c98b003d65ab9af587368728ce8b05b221d37c4f
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:10:46 +0600

    Add Koshary [A] from Codeforces

---
+++ 2227/A - Koshary/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--){
+    cin>>x>>y;
+    if(x%2==1 && y%2==1)cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+ 
+    }
+    return 0;
+}
