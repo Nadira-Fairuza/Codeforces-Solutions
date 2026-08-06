df687c08f030590d12cbca28ca404b4736a34581
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:55:28 +0600

    Add Chemistry [B] from Codeforces

---
+++ 1883/B - Chemistry/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+ 
+        string s;
+        cin >> s;
+ 
+        int freq[26]={0};
+ 
+        for(char c:s){
+            freq[c-'a']++;
+        }
+ 
+        int odd=0;
+ 
+        for(int i=0;i<26;i++){
+            if(freq[i]%2) odd++;
+        }
+ 
+        if(odd-1<=k) cout<<"YES
+";
+        else cout<<"NO
+";
+    }
+ 
+    return 0;
+}
