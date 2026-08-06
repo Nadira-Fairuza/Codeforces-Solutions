8aade4b3ced9514b7b0a4e994533282a5233efbc
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:00:06 +0600

    Add United We Stand [A] from Codeforces

---
+++ 1859/A - United We Stand/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+ 
+        sort(a.begin(), a.end());
+ 
+        int mn = a[0];
+        vector<int> b, c;
+ 
+        for(int x : a) {
+            if(x == mn) b.push_back(x);
+            else c.push_back(x);
+        }
+ 
+        if(c.empty()) {
+            cout << -1 << endl;
+        } else {
+            cout << b.size() << " " << c.size() << endl;
+ 
+            for(int x : b) cout << x << " ";
+            cout << endl;
+ 
+            for(int x : c) cout << x << " ";
+            cout << endl;
+        }
+    }
+}
