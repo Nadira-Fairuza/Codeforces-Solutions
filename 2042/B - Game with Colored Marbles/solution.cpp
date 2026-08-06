0dd0b8572a788efe50bad5308aec6ff99239067e
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:02:26 +0600

    Add Game with Colored Marbles [B] from Codeforces

---
+++ 2042/B - Game with Colored Marbles/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> c(n);
+        for (int i = 0; i < n; i++) {
+            cin >> c[i];
+            c[i]--;
+        }
+ 
+        vector<int> cnt(n);
+ 
+        for (auto x : c)
+            cnt[x]++;
+ 
+        int exactly1 = 0, morethan1 = 0;
+ 
+        for (auto x : cnt) {
+            if (x == 1)
+                exactly1++;
+            else if (x > 1)
+                morethan1++;
+        }
+ 
+        cout << morethan1 + ((exactly1 + 1) / 2) * 2 << "
+";
+    }
+ 
+    return 0;
+}
