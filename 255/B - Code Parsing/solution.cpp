ef850d894e1dd35466e0ad5b87bc506ce49131eb
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:05:17 +0600

    Add Code Parsing [B] from Codeforces

---
+++ 255/B - Code Parsing/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    int cx = 0, cy = 0;
+ 
+    for (char c : s) {
+        if (c == 'x') cx++;
+        else cy++;
+    }
+ 
+    if (cx > cy) {
+        cout << string(cx - cy, 'x');
+    } else {
+        cout << string(cy - cx, 'y');
+    }
+}
