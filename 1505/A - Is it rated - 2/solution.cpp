9617cadcc5c3ca0e779d77784bd1ee0c3caebc65
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:58:06 +0600

    Add Is it rated - 2 [A] from Codeforces

---
+++ 1505/A - Is it rated - 2/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    string s;
+    while (getline(cin, s)) {
+        cout << "NO" << endl;
+        cout.flush(); // important for interactive problems
+    }
+ 
+    return 0;
+}
