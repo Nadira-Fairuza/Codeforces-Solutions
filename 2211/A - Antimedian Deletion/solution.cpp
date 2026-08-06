fbe924dcc96a9abca6712bdcb7f6e547a1221528
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:04:56 +0600

    Add Antimedian Deletion [A] from Codeforces

---
+++ 2211/A - Antimedian Deletion/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> p(n);
+        for (int i = 0; i < n; i++)
+            cin >> p[i];
+ 
+        if (n == 1) {
+            cout << 1 << '
+';
+        } else {
+            for (int i = 0; i < n; i++)
+                cout << 2 << " ";
+            cout << '
+';
+        }
+    }
+}
