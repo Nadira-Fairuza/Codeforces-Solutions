8bdf225b2dc4be921743d56008b8990ab66b9424
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:03:33 +0600

    Add New Year String [A] from Codeforces

---
+++ 2182/A - New Year String/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        string s;
+        cin >> s;
+ 
+        if (s.find("2025") == string::npos ||
+            s.find("2026") != string::npos)
+            cout << 0 << '
+';
+        else
+            cout << 1 << '
+';
+    }
+}
