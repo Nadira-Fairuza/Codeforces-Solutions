000c76e1d798d463e8d0f441a0f32ef210383bfe
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:52:24 +0600

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+    set<char> distinct_chars;
+    for (char c : s) {
+        distinct_chars.insert(c);
+    }
+    if (distinct_chars.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    } else {
+        cout << "IGNORE HIM!" << endl;
+    }
+    return 0;
+}
