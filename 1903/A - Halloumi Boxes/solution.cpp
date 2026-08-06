eb2bea06922997d7edeb7506de7e97e46622ce65
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:55:13 +0600

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+ 
+        vector<int> a(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+ 
+        if(k >= 2) {
+            cout << "YES
+";
+        }
+        else {
+            bool sorted = true;
+ 
+            for(int i = 1; i < n; i++) {
+                if(a[i] < a[i-1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+ 
+            if(sorted) cout << "YES
+";
+            else cout << "NO
+";
+        }
+    }
+}
