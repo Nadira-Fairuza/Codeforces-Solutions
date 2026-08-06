a1bdc45719ad62a07e73292285fae2bbd9f84320
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:07:54 +0600

    Add Seats [B] from Codeforces

---
+++ 2188/B - Seats/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+ 
+    string s;
+    cin >> s;
+ 
+    s = '1' + s + '1';
+ 
+    int ans = 0;
+ 
+    for (int i = 1, l = 0; i <= n; i++) {
+        if (s[i] == '0') {
+            if (s[i - 1] == '1')
+                l = i;
+ 
+            if (s[i + 1] == '1') {
+                int extra = (l == 1) + (i == n);
+                ans += (i - l + 1 + extra) / 3;
+            }
+        } else {
+            ans++;
+        }
+    }
+ 
+    cout << ans << '
+';
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+        solve();
+}
