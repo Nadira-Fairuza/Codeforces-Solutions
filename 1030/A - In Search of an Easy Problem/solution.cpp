9928ec29fee25781d7ece0a9d9eb9f1559545a7e
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:57:35 +0600

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,n;
+    cin>>n;
+    while(n--){
+        cin>>a;
+        if(a==1){
+            cout<<"Hard";
+            return 0;
+        }
+    }
+    cout<<"EASY";
+    return 0;
+}
