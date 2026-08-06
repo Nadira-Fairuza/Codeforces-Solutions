fe37c023dd6cab5ba165a1ef892d15c34811e984
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 10:10:50 +0600

    Add Left and Down [B] from Codeforces

---
+++ 2125/B - Left and Down/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, k;
+        cin >> a >> b >> k;
+ 
+        long long g = gcd(a, b);
+ 
+        if (a / g <= k && b / g <= k)
+            cout << 1 << '
+';
+        else
+            cout << 2 << '
+';
+    }
+}
