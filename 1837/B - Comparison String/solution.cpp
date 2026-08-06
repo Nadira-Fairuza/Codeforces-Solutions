f58db00f8d3695fd7c827d8f489bc670aa9199d5
Nadira Fairuza <nadira.fairuza05@gmail.com>
2026-08-06 09:59:27 +0600

    Add Comparison String [B] from Codeforces

---
+++ 1837/B - Comparison String/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n;
+        cin >> n;
+ 
+        string s;
+        cin >> s;
+ 
+        int cur = 1, mx = 1;
+ 
+        for(int i = 1; i < n; i++){
+            if(s[i] == s[i-1]){
+                cur++;
+            } else {
+                cur = 1;
+            }
+            mx = max(mx, cur);
+        }
+ 
+        cout << mx + 1 << endl;
+    }
+ 
+    return 0;
+}
